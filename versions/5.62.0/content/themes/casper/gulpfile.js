65c3f187e5a67f02c94d6011eab553d52dbbec50
mafanyya <mafanya.nyaa@gmail.com>
2023-09-13 23:50:06 +0200

    error try 2.3

--- versions/5.62.0/content/themes/casper/gulpfile.js
+++ versions/5.62.0/content/themes/casper/gulpfile.js
@@ -90,15 +90,15 @@ function zipper(done) {
     ], handleError(done));
 }
 
-const cssWatcher = () => watch('assets/css/**', css);
-const jsWatcher = () => watch('assets/js/**', js);
-const hbsWatcher = () => watch(['*.hbs', 'partials/**/*.hbs'], hbs);
-const watcher = parallel(cssWatcher, jsWatcher, hbsWatcher);
-const build = series(css, js);
-
-exports.build = build;
-exports.zip = series(build, zipper);
-exports.default = series(build, serve, watcher);
+// const cssWatcher = () => watch('assets/css/**', css);
+// const jsWatcher = () => watch('assets/js/**', js);
+// const hbsWatcher = () => watch(['*.hbs', 'partials/**/*.hbs'], hbs);
+// const watcher = parallel(cssWatcher, jsWatcher, hbsWatcher);
+// const build = series(css, js);
+//
+// exports.build = build;
+// exports.zip = series(build, zipper);
+// exports.default = series(build, serve, watcher);
 
 exports.release = async () => {
     // @NOTE: https://yarnpkg.com/lang/en/docs/cli/version/
